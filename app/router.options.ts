f613709b001b6fca29a18dc656677013e09146e1
F0m1k23 <vitaliifomenko153@gmail.com>
2025-11-20 22:54:46 +0100

    fix node

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,8 +1,6 @@
 export default {
 	async scrollBehavior(to, from, savedPosition) {
-		// если есть якорь (#about, #news и т.д.)
 		if (to.hash) {
-			// пробуем найти элемент не сразу, а подождать пока DOM смонтируется
 			return new Promise(resolve => {
 				const checkExists = () => {
 					const el = document.querySelector(to.hash)
@@ -10,10 +8,9 @@ export default {
 						resolve({
 							el,
 							behavior: 'smooth',
-							top: 80, // отступ под шапку
+							top: 80,
 						})
 					} else {
-						// если элемента нет — пробуем снова через 50ms
 						setTimeout(checkExists, 50)
 					}
 				}
@@ -22,7 +19,6 @@ export default {
 			})
 		}
 
-		// обычный скролл
 		return savedPosition || { top: 0 }
 	},
 }
